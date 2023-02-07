98ced18b31b7e56265667202b62eda8cc779e4f8
ski235 <110300995+ski235@users.noreply.github.com>
2023-02-07 16:40:55 +0100

    cool

---
+++ code2.js
@@ -0,0 +1,8 @@
+function username (){
+    let user_name =prompt("enter user_name ")
+    let password=prompt("enter password")
+    if (user_name=="admin"&&password==2352323){
+        window.location.href='admin.html'
+    }
+    
+    }
