57e455533f0c3343fc1278c67e7734048e4d8f2c
ski235 <johndoe@example.com>
2023-03-04 13:50:41 +0100

    c

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 
-var nom=prompt("Quel est votre nom")
+var nom=prompt("Quel est votre nom/What's your name")
 alert("Bienvenue sur notre site/Welcome to our website"+nom)
 console.log(nom)
+function username (){
+    let user_name =prompt("enter user_name ")
+    let password=prompt("enter password")
+    if (user_name=="admin"&&password==2352323){
+        window.location.href='admin.html'
+    }
+    
+    }
+
